036df035da120172715138772e28cdc39124704c
zinnnn37 <zinnnn37@gmail.com>
2025-10-26 20:04:53 +0900

    [chore] rename

--- Payment/src/main/java/com/study/payment/PaymentApplication.java
+++ Payment/src/main/java/com/study/payment/PaymentApplication.java
@@ -1,13 +1,13 @@
-package com.study.saga;
+package com.study.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SagaApplication {
+public class PaymentApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SagaApplication.class, args);
+		SpringApplication.run(PaymentApplication.class, args);
 	}
 
 }
--- Payment/src/test/java/com/study/payment/SagaApplicationTests.java
+++ Payment/src/test/java/com/study/payment/SagaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.study.saga;
+package com.study.payment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
